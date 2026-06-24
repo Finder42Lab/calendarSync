571c3a9e767f75d1e07ce9f2d9ca781b3dbc7900
username <ddanila.supp0rt.office@yandex.ru>
2026-06-24 18:33:33 +0300

    Поправил логирование

--- project/notify.py
+++ project/notify.py
@@ -1,7 +1,6 @@
 import sys
 
 import datetime
-import logging
 from loguru import logger
 
 from caldav_api import CalDavCalendar, CalDavEvent
@@ -45,9 +44,6 @@ def get_hourly_events(calendar: CalDavCalendar):
     period_start = datetime.datetime.now().replace(minute=0, second=0, microsecond=0) + datetime.timedelta(hours=1)
     period_end = period_start + datetime.timedelta(minutes=1)
 
-    logging.info(f'{period_start} - {period_end}')
-
-
     events = calendar.events(period_start, period_end)
 
     return events
@@ -57,7 +53,7 @@ def get_now_events(calendar: CalDavCalendar):
     period_start = datetime.datetime.now().replace(minute=0, second=0, microsecond=0) + datetime.timedelta(minutes=2)
     period_end = period_start + datetime.timedelta(minutes=1)
 
-    logging.info(f'{period_start} - {period_end}')
+    logger.info(f'{period_start} - {period_end}')
 
     events = calendar.events(period_start, period_end)
 
